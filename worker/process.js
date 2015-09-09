114755e9b914cfd8e65d1af6bf60c36d3d830a44
Mark Stratmann <mark@stratmann.me.uk>
2015-09-09 16:17:15 +0100

    Process.js complete

--- worker/process.js
+++ worker/process.js
@@ -2,13 +2,16 @@ console.log('Start');
 
 var region = process.env.AWS_REGION;
 var bucket  = process.env.CONFIG_FILE_BUCKET;
-var topic = process.env.CONFIG_FILE_UPDATE_TOPIC;
+var queue = process.env.CONFIG_FILE_UPDATE_QUEUE;
 var folder = process.env.CONFIG_FILE_FOLDER;
 
 var aws = require('aws-sdk');
 var _ = require('underscore');
 var exec = require('child_process').exec;
 var s3 = new aws.S3({
+  region: region
+});
+var sqs = new aws.SQS({
   region: region
 });
 var fs = require('fs');
@@ -30,12 +33,40 @@ var downloadAllFiles = function() {
   }
 };
 
+var needUpdate = function (fn) {
+  var params = {
+    QueueUrl: queue,
+    AttributeNames: [
+      'ApproximateNumberOfMessages'
+    ]
+  };
+  sqs.getQueueAttributes(params, function(err, data) {
+    console.log('getQueueAttributes');
+    console.log(err, data);
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+    var result = false;
+    if (!_.isUndefined(data.Attributes)) {
+      console.log(data.Attributes);
+      if (parseInt(data.Attributes.ApproximateNumberOfMessages) > 0) {
+        result = true;
+      }
+    }
+    fn(result);
+  });
+};
+
 downloadAllFiles();
 
-// setInterval(function() {
-//   console.log('Checking for Job Changes');
-//   setUpJobs();
-//   console.log(jobs);
-// }, 60000);
+setInterval(function() {
+  console.log('Checking for File Changes');
+  needUpdate(function(need_update){
+    if (need_update) {
+      downloadAllFiles();
+    }
+  });
+
+}, 60000);
+
 
 console.log("Exit");
